1a734a3a356310f9836773d1d33f48b844f18c12
anirudh <anirudhsai027@gmail.com>
2024-03-20 23:51:07 +0100

    added text to speech german and changes to speech to text german

--- utils.py
+++ utils.py
@@ -11,8 +11,27 @@ def speech_to_text_german(api_key, audio_file_path):
     '''
     client = OpenAI(api_key=api_key)
     audio_file= open(audio_file_path, "rb")
-    translation = client.audio.translations.create(
+    translation = client.audio.transcriptions.create(
         model="whisper-1", 
         file=audio_file
     )
     return translation.text
+
+def text_to_speech_german(api_key, text, return_audio_file_path):
+    '''
+    params: 
+    api_key: str: openai api key
+    text: str: text to convert to speech
+    return_audio_file_path: str: path to save the audio file
+    
+    returns:
+    str: path to the audio file
+    '''
+    client = OpenAI(api_key=api_key)
+    response = client.audio.speech.create(
+         model="tts-1",
+        voice="alloy",
+        input=text
+    )
+    response.stream_to_file(return_audio_file_path)
+    
